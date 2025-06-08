ab697d46cc9d0beb669373d567040d93f351139e
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-06-08 13:22:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source to get available availability zones in the region
+data "aws_availability_zones" "available" {
+  state = "available"
+}
